d7577379de909bfc982d77e8474d245098b7965a
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-10-20 22:41:23 +0300

    исправил написание скобок

--- lesson_02/main.js
+++ lesson_02/main.js
@@ -1,20 +1,21 @@
 'use strick'
 
-{const title = 'apple';
+{ const title = 'apple';
 const amount = 5;
 const category = 'fruit';
 const price = 100;
 console.log(title); //наименование товара
 
 const total = amount * price;
-console.log(total)} // общая сумма товара
+console.log(total) // общая сумма товара
+} 
 
-{const title = 'tomato';
+{ const title = 'tomato';
 const amount = 3;
 const category = 'vegetables';
 const price = 150;
 console.log(title); //наименование товара
 
 const total = amount * price;
-console.log(total)} // общая сумма товара
+console.log(total) } // общая сумма товара
 
