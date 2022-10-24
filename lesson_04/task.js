46d1cca2e3a489b3e1cc8c55431f7b586c306ecb
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-10-24 22:56:01 +0300

    2 задание 4 урок

---
+++ lesson_04/task.js
@@ -0,0 +1,17 @@
+'use strick'
+
+// Урок 4 (задание №2)
+{
+const rain = Math.random();
+const rainResult = Math.round(rain);
+console.log(rainResult);
+if (rainResult >= 1 ) {
+    alert('Пошёл дождь. Возьмите зонт!')
+    
+} else {
+    alert('Дождя нет!')
+    
+}
+}
+
+//Урок 4 (задание №3)
