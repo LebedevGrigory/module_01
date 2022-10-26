5d042a7ec17792624dbfc3d940def11bbdfd69a4
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-10-26 15:43:13 +0300

    add task2 lesson5

--- lesson_05/task2.js
+++ lesson_05/task2.js
@@ -1,1 +1,9 @@
 'use strick'
+
+String.prototype.firstLetterToUppercase = function() {
+    return this[0].toUpperCase() + this.slice(1);
+}
+
+const name = 'регистр Изменился';
+
+console.log( name.firstLetterToUppercase());
