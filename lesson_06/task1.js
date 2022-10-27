7ea74ea36e8aad49f68768cd3f8a822f02e5cc56
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-10-27 21:58:21 +0300

    add lesson6

--- lesson_06/task1.js
+++ lesson_06/task1.js
@@ -1,4 +1,5 @@
 'use strick'
+
 let n = 1;
 
 
@@ -6,6 +7,6 @@ for (let i = 1; i < 11; i++){
     console.log('------------------');
     for (let y = 1; y < 11; y++){
          console.log( `${i} x ${y} = ${i ** y} `)
-   
+
     }
 }
